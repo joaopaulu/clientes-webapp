b016c3bfe49c4b8caae69ab4ea005b0d66eb9058
João Paulo <jptick@gmail.com>
2021-01-12 20:26:58 -0300

    Update database migrations

--- backend/src/main/resources/db/migration/V01__criar_e_registrar_clientes.sql
+++ backend/src/main/resources/db/migration/V01__criar_e_registrar_clientes.sql
@@ -1,17 +1,17 @@
 CREATE TABLE cliente(
-    id bigserial not null,
+    id serial not null,
     cpf varchar(11) NOT NULL,
     data_cadastro date,
     nome varchar(150) NOT NULL,
     primary key (id)
 );
 
-INSERT INTO cliente (id, cpf, data_cadastro, nome) VALUES (1,'00479309183','2020-12-29','João Paulo');
-INSERT INTO cliente (id, cpf, data_cadastro, nome) VALUES (2,'96928370092','2020-12-29','Thiago');
-INSERT INTO cliente (id, cpf, data_cadastro, nome) VALUES (3,'32296356044','2020-12-29','Rafael');
-INSERT INTO cliente (id, cpf, data_cadastro, nome) VALUES (4,'57517508024','2020-12-29','Rodrigo');
-INSERT INTO cliente (id, cpf, data_cadastro, nome) VALUES (5,'78037949044','2020-12-29','Camila');
-INSERT INTO cliente (id, cpf, data_cadastro, nome) VALUES (6,'93598591098','2020-12-29','Fernanda');
-INSERT INTO cliente (id, cpf, data_cadastro, nome) VALUES (7,'93598591098','2020-12-29','Fernando');
-INSERT INTO cliente (id, cpf, data_cadastro, nome) VALUES (8,'48097491001','2020-12-29','Marina');
-INSERT INTO cliente (id, cpf, data_cadastro, nome) VALUES (9,'95485751083','2020-12-29','Geovana');
+INSERT INTO cliente (cpf, data_cadastro, nome) VALUES ('00479309183','2020-12-29','João Paulo');
+INSERT INTO cliente (cpf, data_cadastro, nome) VALUES ('96928370092','2020-12-29','Thiago');
+INSERT INTO cliente (cpf, data_cadastro, nome) VALUES ('32296356044','2020-12-29','Rafael');
+INSERT INTO cliente (cpf, data_cadastro, nome) VALUES ('57517508024','2020-12-29','Rodrigo');
+INSERT INTO cliente (cpf, data_cadastro, nome) VALUES ('78037949044','2020-12-29','Camila');
+INSERT INTO cliente (cpf, data_cadastro, nome) VALUES ('93598591098','2020-12-29','Fernanda');
+INSERT INTO cliente (cpf, data_cadastro, nome) VALUES ('93598591098','2020-12-29','Fernando');
+INSERT INTO cliente (cpf, data_cadastro, nome) VALUES ('48097491001','2020-12-29','Marina');
+INSERT INTO cliente (cpf, data_cadastro, nome) VALUES ('95485751083','2020-12-29','Geovana');
--- backend/src/main/resources/db/migration/V02__criar_e_registrar_servicos.sql
+++ backend/src/main/resources/db/migration/V02__criar_e_registrar_servicos.sql
@@ -1,5 +1,5 @@
 CREATE TABLE servico_prestado(
-    id bigserial not null,
+    id serial not null,
     descricao varchar(150) NOT NULL,
     valor decimal(19,2),
     id_cliente int,
@@ -8,10 +8,10 @@ CREATE TABLE servico_prestado(
     FOREIGN KEY (id_cliente) references cliente(id)
 );
 
-INSERT INTO servico_prestado (id, descricao, valor, id_cliente, data) VALUES (1,'Formatar PC',100.00,1,'2020-12-20');
-INSERT INTO servico_prestado (id, descricao, valor, id_cliente, data) VALUES (2,'Consertar o teclado',50.00,2,'2020-01-20');
-INSERT INTO servico_prestado (id, descricao, valor, id_cliente, data) VALUES (3,'Consertar o mouse',30.00,3,'2020-02-20');
-INSERT INTO servico_prestado (id, descricao, valor, id_cliente, data) VALUES (4,'Consertar o celular',90.00,4,'2020-03-20');
-INSERT INTO servico_prestado (id, descricao, valor, id_cliente, data) VALUES (5,'Consertar a TV',500.00,5,'2020-04-20');
-INSERT INTO servico_prestado (id, descricao, valor, id_cliente, data) VALUES (6,'Consertar o Secador',50.00,5,'2020-05-20');
-INSERT INTO servico_prestado (id, descricao, valor, id_cliente, data) VALUES (7,'Consertar o microondas',50.00,7,'2020-05-20');
+INSERT INTO servico_prestado (descricao, valor, id_cliente, data) VALUES ('Formatar PC',100.00,1,'2020-12-20');
+INSERT INTO servico_prestado (descricao, valor, id_cliente, data) VALUES ('Consertar o teclado',50.00,2,'2020-01-20');
+INSERT INTO servico_prestado (descricao, valor, id_cliente, data) VALUES ('Consertar o mouse',30.00,3,'2020-02-20');
+INSERT INTO servico_prestado (descricao, valor, id_cliente, data) VALUES ('Consertar o celular',90.00,4,'2020-03-20');
+INSERT INTO servico_prestado (descricao, valor, id_cliente, data) VALUES ('Consertar a TV',500.00,5,'2020-04-20');
+INSERT INTO servico_prestado (descricao, valor, id_cliente, data) VALUES ('Consertar o Secador',50.00,5,'2020-05-20');
+INSERT INTO servico_prestado (descricao, valor, id_cliente, data) VALUES ('Consertar o microondas',50.00,7,'2020-05-20');
